c34b9190661334c86f7d757a52f108a8a36ff34b
AndersSjo <anders@lagenhetsbyte.se>
2020-12-28 02:20:15 +0100

    test

--- src/templates/startsida.js
+++ src/templates/startsida.js
@@ -4,7 +4,7 @@ import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+export const AboutPageTemplate4 = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content;
 
   return (
@@ -25,7 +25,7 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   );
 };
 
-AboutPageTemplate.propTypes = {
+AboutPageTemplate4.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
   contentComponent: PropTypes.func,
@@ -36,7 +36,7 @@ const AboutPage = ({ data }) => {
 
   return (
     <Layout>
-      <AboutPageTemplate
+      <AboutPageTemplate4
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
